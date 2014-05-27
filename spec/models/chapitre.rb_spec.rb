58a4327c7752b562ac74ea29e967a9d0c9b8fc57
yc005232 <dia5@hotmail.fr>
2014-05-28 01:55:05 +0200

    ajout de test pour le model du chapitre

--- spec/models/chapitre.rb_spec.rb
+++ spec/models/chapitre.rb_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Chapitre do
-  pending "add some examples to (or delete) #{__FILE__}"
+ before do
+	@chapitre=Chapitre.new(numero: 0 ,titre: "Example Chapitre")
+  end
+
+  subject {@chapitre}
+  
+
+  it {should respond_to(:titre)} 
+
+  it {should be_valid}
+
+  describe "when titre is not present" do
+     before {@chapitre.titre=" "}
+     it {should_not be_valid}
+  end
+  describe "when number isn't an integer" do
+     before { @chapitre.numero = "a" }
+     it { should_not be_valid }
+  end
 end
