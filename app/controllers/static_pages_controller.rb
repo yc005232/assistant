23c7cde154fd6f4e0240f58b0d15b97d4b3d7f89
yc005232 <dia5@hotmail.fr>
2014-05-28 04:55:24 +0200

    generation des pages statics suplementaire necessaire .

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def recappersonnes
+  end
+
+  def recapchapitre
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Assistant::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/recappersonnes"
+  get "static_pages/recapchapitre"
   resources :personnes
 
   resources :scenes
