b50584a6a15eb141a5103e8e7c4b99c3f5d13bb6
yc005232 <dia5@hotmail.fr>
2014-05-28 06:03:08 +0200

    routage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Assistant::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/recappersonnes"
-  get "static_pages/recapchapitre"
-  get "static_pages/about"
-  get "static_pages/contact"
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/recappersonnes', to: 'static_pages#recappersonnes', via: 'get'
+  match '/recapchapitre', to: 'static_pages#recapchapitre', via: 'get'
   resources :personnes
 
   resources :scenes
